f2f84b994e46a77177a6a85bda69d4acb983f552
aallliiii <malihumayon@gmail.com>
2024-06-23 22:12:10 +0500

    Half

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
-
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true}));
 app.listen(port, ()=>
 {
     console.log("Listening on port " + port);
@@ -16,4 +17,10 @@ app.listen(port, ()=>
 app.get("/", (req,res) =>
 {
     res.render(__dirname + "/views/index.ejs");
+})
+
+app.post("/submit-form", (req, res) =>
+{
+    console.log((req.body.city).toLowerCase());
+    
 })
